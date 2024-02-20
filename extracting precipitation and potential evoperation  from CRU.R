deb414c52458bc60744f89d785cceb6b9e1470b2
Qingqing Chen <chqq365@gmail.com>
2024-02-20 16:55:17 +0100

    major revision using data from 1901 to 2022

--- extracting precipitation and potential evoperation  from CRU.R
+++ extracting precipitation and potential evoperation  from CRU.R
@@ -1,6 +1,6 @@
 
 ## download, raster and intersect CRU timeseries monthly weather data for NutNet
-## climate data downloaded from https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_4.06/cruts.2205201912.v4.06/pet/
+## climate data downloaded from https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_4.07/cruts.2304141047.v4.07/pre/
 ## read this article for introduction of the data https://www.nature.com/articles/s41597-020-0453-3
 # useful background info netcdf datasets: http://www.image.ucar.edu/GSP/Software/Netcdf/
 
@@ -13,18 +13,18 @@ require(lubridate)
 require(chron)
 require(raster)
 require(tidyverse)
-
-setwd('H:/data from NutNet/SPEI/')
+dir.data<-"C:/Users/chqq3/work/NutNet data/"
+setwd(dir.data)
 
 #  load NutNet geolocation data 
-sites <- read.csv('comb-by-plot-clim-soil-2022-11-15.csv', header=T, sep=",")
+sites <-read.csv("comb-by-plot-clim-soil-diversity_2023-11-07.csv", header = T) 
 sites2<-sites%>%dplyr::select("site_code", 'longitude','latitude')%>%distinct()
 
 # take a variable to use: pet, pre, tmp, tmx, tmn, here focus on pre and pet
 for (variable in c("pre", "pet")){
- 
+ variable<-"pet"
 cruv <- variable
-ncfile <- paste0('cru_ts4.06.1901.2021.',cruv,'.dat.nc')
+ncfile <- paste0('cru_ts4.07.1901.2022.',cruv,'.dat.nc')
 nc <- nc_open(ncfile)
 print(nc)
 
@@ -47,11 +47,10 @@ points(sp,cex=1.5,pch=16)
 
 # make brick of all layers
 cru.brick <- brick(ncfile)
-
 nlayers(cru.brick) == nt # should equal number of months in time series
 
 # extract each layer with lat-long points
-NutNet.dat <- extract(cru.brick, sp)
+NutNet.dat <- raster::extract(cru.brick, sp)
 dim(NutNet.dat) # sites=rows, times = cols
 rownames(NutNet.dat) <- sites2$site_code
 colnames(NutNet.dat) <- as.character(realt)
@@ -63,12 +62,12 @@ misng <- rownames(NutNet.dat[is.na(NutNet.dat[,1]),])
 # add lon and lat data for all NutNet sites 
 NutNet.dat1<-as.data.frame(NutNet.dat)
 NutNet.dat1$site_code<-rownames(NutNet.dat1)
-NutNet.dat.long <- NutNet.dat1%>%pivot_longer(cols = 1:1452, names_to = "date_time")%>%
+NutNet.dat.long <- NutNet.dat1%>%pivot_longer(cols = 1:1464, names_to = "date_time")%>%
   mutate(unit=ncatt_get(nc,cruv,'units')$value, years=substr(date_time, 8, 11), months=substr(date_time, 1,3), date=substr(date_time, 5,6)) %>%
   merge(sites2, by=c("site_code"))
 
 # output the data
- write.csv(NutNet.dat.long, paste0('CRU-monthly-',cruv,'-1901-2021.csv'),row.names=F)
+ write.csv(NutNet.dat.long, paste0('CRU-monthly-',cruv,'-1901-2022.csv'),row.names=F)
 
 }
 
